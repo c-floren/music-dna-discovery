ffc6264868592b58b3b13ed0ef1b787c72c71f6d
Chabelee Florentino <chabelee.florentino@gmail.com>
2026-06-25 19:01:06 -0400

    Initial commit: Spring Boot backend boots clean against PostgreSQL

---
+++ discovery-engine/src/main/java/com/musicdna/discovery_engine/DiscoveryEngineApplication.java
@@ -0,0 +1,13 @@
+package com.musicdna.discovery_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryEngineApplication.class, args);
+	}
+
+}
---
+++ discovery-engine/src/test/java/com/musicdna/discovery_engine/DiscoveryEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicdna.discovery_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
